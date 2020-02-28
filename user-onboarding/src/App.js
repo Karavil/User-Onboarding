bcec58a154179e4fef019bde378d7d00f0f7f422
Alex Karavil <alex@karavil.io>
2020-02-28 09:36:21 -0500

    Added conditional styling to the signup list.

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,21 +1,40 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import SignupForm from "./components/SignupForm";
 
 import "./App.css";
 
 const SignupList = ({ signups }) => {
+   const CardList = styled.div`
+      margin: 100px 0;
+      border: 2px grey solid;
+      min-height: 50px;
+
+      .no-signups {
+         color: grey;
+      }
+   `;
+
+   const Card = styled.div`
+      border: 2px solid black;
+      background: green;
+      color: white;
+   `;
    const cards = signups.map(user => {
-      return (<div className="card" key={Date.now()}>
-         <h1>
-            {`${user.firstName} ${user.lastName}`}
-         </h1>
-         <h2>{user.email}</h2>
-         <h2>{user.mobileNumber}</h2>
-      </div>)
-   })
+      return (
+         <Card key={Date.now()}>
+            <h2>{`${user.firstName} ${user.lastName}`}</h2>
+            <h3>{user.email}</h3>
+            <h3>{user.mobileNumber}</h3>
+         </Card>
+      );
+   });
    return (
-      <div className="cardList">{cards}</div>
-   )
+      <CardList>
+         <h1>Registered Users:</h1>
+         {signups.length > 0 ? cards : <h2 className='no-signups'>No signups at the moment.</h2>}
+      </CardList>
+   );
 };
 
 function App() {
@@ -28,7 +47,7 @@ function App() {
    return (
       <div className="App">
          <SignupForm addNewSignup={addNewSignup} />
-         <SignupList signups={signups}/>
+         <SignupList signups={signups} />
       </div>
    );
 }
